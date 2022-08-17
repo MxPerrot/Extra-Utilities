ff880f54c0cf588e7df41db0e71a5a0ccdac93f6
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-17 02:58:57 +0200

    Not finished

--- src/auto/AutoPlugin.java
+++ src/auto/AutoPlugin.java
@@ -1,83 +1,55 @@
 package auto;
 
-import arc.*;
+import arc.Events;
 import arc.util.*;
+import arc.util.CommandHandler;
+import arc.util.Log;
+import arc.util.Strings;
 import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
+import mindustry.game.EventType;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.net.Administration.*;
-import mindustry.world.blocks.storage.*;
+import mindustry.net.Administration;
+import static arc.util.Log.info;
 
 public class AutoPlugin extends Plugin{
+    private int playerCount = 0;
+    //private String playtime = ;
 
     @Override
     public void init(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
-                //player is the unit controller
-                Player player = event.builder.getPlayer();
+        playtime = Core.settings.getString("playtime", playtime);
 
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
+        Events.on(EventType.WorldLoadEvent.class, event -> {
+            if (playerCount == 0 && Vars.state.ServerPaused == false) {
+                String message = Strings.format("Auto-Pause : ON");
+                Vars.state.serverPaused = true;
             }
         });
-
-        //add a chat filter that changes the contents of all messages
-        //in this case, all instances of "heck" are censored
-        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
-
-        //add an action filter for preventing players from doing certain things
-        Vars.netServer.admins.addActionFilter(action -> {
-            //random example: prevent blast compound depositing
-            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
-                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
-                return false;
+        Events.on(EventType.PlayerJoin.class, event -> {
+            playerCount -= 1;
+            if (playerCount == 0 && Vars.state.serverPaused == false) {
+                String message = Strings.format("Auto-Pause : OFF");
+                Vars.state.serverPaused = false;
             }
-            return true;
+            playerCount += 1;
         });
-    }
-
-    //register commands that run on the server
-    @Override
-    public void registerServerCommands(CommandHandler handler){
-        handler.register("reactors", "List all thorium reactors in the map.", args -> {
-            for(int x = 0; x < Vars.world.width(); x++){
-                for(int y = 0; y < Vars.world.height(); y++){
-                    //loop through and log all found reactors
-                    //make sure to only log reactor centers
-                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor && Vars.world.tile(x, y).isCenter()){
-                        Log.info("Reactor at @, @", x, y);
-                    }
-                }
+        Events.on(EventType.PlayerLeave.class, event -> {
+            playerCount -= 1;
+            if (playerCount == 0 && Vars.state.serverPaused == false) {
+                String message = Strings.format("Auto-Pause : ON");
+                Vars.state.serverPaused = false;
             }
         });
     }
 
-    //register commands that player can invoke in-game
     @Override
     public void registerClientCommands(CommandHandler handler){
-
-        //register a simple reply command
-        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
-        });
-
-        //register a whisper command which can be used to send other players messages
-        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
-            //find player by name
-            Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
-
-            //give error message with scarlet-colored text if player isn't found
-            if(other == null){
-                player.sendMessage("[scarlet]No player by that name found!");
-                return;
-            }
-
-            //send the other player a message, using [lightgray] for gray text color and [] to reset color
-            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
-        });
+        handler.<Player>register("playtime", "Get your playtime on the server.", (arg, player) -> {
+            player.sendMessage("You have", playtime, "playtime on this server");
+        }
+    }
+    private void save(){
+        Core.settings.put("playtime", playtime);
     }
 }
