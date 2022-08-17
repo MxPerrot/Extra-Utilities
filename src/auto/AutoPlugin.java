694cf7123878055fba283bac107256f82f155386
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-17 19:58:34 +0200

    Update AutoPlugin.java

--- src/auto/AutoPlugin.java
+++ src/auto/AutoPlugin.java
@@ -11,6 +11,7 @@ import mindustry.game.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.core.GameState;
 import mindustry.net.Administration.*;
 import static arc.util.Log.info;
 
@@ -21,13 +22,13 @@ public class AutoPlugin extends Plugin{
     public void init(){
         Events.on(EventType.WorldLoadEvent.class, event -> {
             if (playerCount == 0 && Vars.state.serverPaused == false) {
-                String message = Strings.format("[Auto-Pause] ON");
+                String message = Strings.format("[Auto-Pause] [green]ON");
                 Vars.state.serverPaused = true;
             }
         });
         Events.on(EventType.PlayerJoin.class, event -> {
             if (playerCount == 0 && Vars.state.serverPaused == false) {
-                String message = Strings.format("[Auto-Pause] OFF");
+                String message = Strings.format("[Auto-Pause] [red]OFF");
                 Vars.state.serverPaused = false;
             }
             playerCount += 1;
@@ -35,24 +36,24 @@ public class AutoPlugin extends Plugin{
         Events.on(EventType.PlayerLeave.class, event -> {
             playerCount -= 1;
             if (playerCount == 0 && Vars.state.serverPaused == false) {
-                String message = Strings.format("[Auto-Pause] ON");
+                String message = Strings.format("[Auto-Pause] [green]ON");
                 Vars.state.serverPaused = false;
             }
         });
     }
     @Override
     public void registerClientCommands(CommandHandler handler) {
-        handler.<Player>register("pause", "<on/off>", "Pause/Unpause the game.", (arg, player) -> {
-            if (arg.length == 0) {
-                player.sendMessage("[scarlet]Error: Argument 'on' or 'off' needed");
+        handler.<Player>register("pause", "<on/off>", "Pause/Unpause the game.", (args, player) -> {
+            if (arg[0].length == 0) {
+                player.sendMessage("[scarlet]Error: Argument 'on' or 'off' required.");
             }
-            if (arg[0].equals("on")) {
+            if (arg[0].equals("on") && Vars.state.serverPaused == false) {
                 Vars.state.serverPaused = true;
-                String message = Strings.format("[Pause] ON");
+                String message = Strings.format("[Pause] [green]ON");
             }
-            if (arg[0].equals("off")) {
+            if (arg[0].equals("off") && Vars.state.ServerPaused == true) {
                 Vars.state.serverPaused = false;
-                String message = Strings.format("[Pause] OFF");
+                String message = Strings.format("[Pause] [red]OFF");
             }
         });
     } 
