75892d14dc6fe39f0e0b014abed22b8a878f90f5
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-24 21:22:40 +0200

    update ExtraUtilitiesPlugin.java

--- src/main/java/ExtraUtilitiesPlugin.java
+++ src/main/java/ExtraUtilitiesPlugin.java
@@ -28,20 +28,20 @@ public class ExtraUtilitiesPlugin extends mindustry.mod.Plugin {
 
     public void init(){
         Events.on(EventType.WorldLoadEvent.class, e -> {
-            if (Vars.state.serverPaused == false && Groups.player.size() == 0) {
-                Vars.state.serverPaused = true;
+            if (state.serverPaused == false && Groups.player.size() == 0) {
+                state.serverPaused = true;
                 Call.sendMessage("[Auto-Pause] [green]ON");
             }
         });
         Events.on(EventType.PlayerJoin.class, e -> {
-            if (Vars.state.serverPaused == true && Groups.player.size() == 1) {
-                Vars.state.serverPaused = false;
+            if (state.serverPaused == true && Groups.player.size() == 1) {
+                state.serverPaused = false;
                 Call.sendMessage("[Auto-Pause] [red]OFF");
             }
         });
         Events.on(EventType.PlayerLeave.class, e -> {
-            if (Vars.state.serverPaused == false && Groups.player.size()-1 == 0) {
-                Vars.state.serverPaused = true;
+            if (state.serverPaused == false && Groups.player.size()-1 == 0) {
+                state.serverPaused = true;
                 Call.sendMessage("[Auto-Pause] [green]ON");
             }
         });
@@ -53,14 +53,14 @@ public class ExtraUtilitiesPlugin extends mindustry.mod.Plugin {
             if (arg.length == 0) {
                 player.sendMessage("[scarlet]Error: Argument 'on' or 'off' required.");
             }
-            if (arg[0].equals("on") && Vars.state.serverPaused == false) {
-                Vars.state.serverPaused = true;
+            if (arg[0].equals("on") && state.serverPaused == false) {
+                state.serverPaused = true;
                 Call.sendMessage("[Pause] [green]ON");
             }
             else {
                 player.sendMessage("[scarlet]Server is already paused.");
             }
-            if (arg[0].equals("off") && Vars.state.serverPaused == true) {
+            if (arg[0].equals("off") && state.serverPaused == true) {
                 state.serverPaused = false;
                 Call.sendMessage("[Pause] [red]OFF");
             }
