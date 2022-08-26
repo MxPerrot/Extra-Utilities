ebb01e5efba6d596c216066f02e7235d7fc03115
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-26 04:56:51 +0200

    Rename Commands.java to EUCommands.java

---
+++ core/src/extra/EUCommands.java
@@ -0,0 +1,50 @@
+package extra.commands;
+
+import arc.*;
+import arc.func.*;
+import arc.util.*;
+import mindustry.core.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.net.*;
+import static mindustry.Vars.player;
+import static mindustry.Vars.state;
+import static mindustry.Vars.netServer;
+import static mindustry.Vars.netClient;
+
+public class Commands {
+    public Command() {
+        Log.info("E-U |--> Commands loaded.");
+    }
+
+    public void registerClientCommands(final CommandHandler handler) {
+        handler.<Player>register("pause", "<on/off>", "Pause/Unpause the game.", (arg, player) -> {
+            if (arg[0].equals("on")) {
+                if (state.serverPaused == false) {
+                    state.serverPaused = true;
+                    Call.sendMessage("[#bebebe]Server [green]paused [#bebebe]by [#ffffff] " + player.name + ".");
+                }
+                else if (state.serverPaused == true) {
+                    player.sendMessage("[scarlet]Server is already paused.");
+                }
+            }
+            if (arg[0].equals("off")) {
+                if (state.serverPaused == true) {
+                    state.serverPaused = false;
+                    Call.sendMessage("[#bebebe]Server [red]unpaused [#bebebe]by [#ffffff] " + player.name + ".");
+                }
+                else if (state.serverPaused == false) {
+                    player.sendMessage("[scarlet]Server is already unpaused.");
+                }
+            }
+            if (!(arg[0].equals("on") || arg[0].equals("off"))) {
+                player.sendMessage("[scarlet]Need argument 'on' or 'off'.");
+            }
+        });
+    }
+
+    public void registerServerCommands(final CommandHandler handler) {
+        
+    }
+}
