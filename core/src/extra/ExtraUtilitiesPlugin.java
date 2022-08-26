1fe2b911512dc26934a18e94d7494479eb45e036
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-26 04:19:16 +0200

    update ExtraUtilitiesPlugin.java

--- core/src/extra/ExtraUtilitiesPlugin.java
+++ core/src/extra/ExtraUtilitiesPlugin.java
@@ -3,11 +3,11 @@ package extra;
 import arc.util.*;
 import extra.Commands;
 
-
 public class ExtraUtilitiesPlugin extends mindustry.mod.Plugin {
     public ExtraUtilitiesPlugin() {
         Log.info("|--> Extra-Utilities is loading...");
     }
+    @Override
     public void loadContent() {
         new Commands().load();
     }
