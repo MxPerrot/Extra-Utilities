fcb53a8354a83c9c558935edd2f96e08ab2eed5f
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-26 13:07:55 +0200

    update EUCommands.java

--- core/src/extra/EUCommands.java
+++ core/src/extra/EUCommands.java
@@ -18,36 +18,34 @@ public class EUCommands {
         Log.info("E-U |--> Commands are loading...");
     }
     public static void load() {
-        @Override
-        public void registerClientCommands(CommandHandler handler) {
-            handler.<Player>register("pause", "<on/off>", "Pause/Unpause the game.", (arg, player) -> {
-                if (arg[0].equals("on")) {
-                    if (state.serverPaused == false) {
-                        state.serverPaused = true;
-                        Call.sendMessage("[#bebebe]Server [green]paused [#bebebe]by [#ffffff] " + player.name + ".");
-                    }
-                    else if (state.serverPaused == true) {
-                        player.sendMessage("[scarlet]Server is already paused.");
-                    }
+    public void registerClientCommands(CommandHandler handler) {
+        handler.<Player>register("pause", "<on/off>", "Pause/Unpause the game.", (arg, player) -> {
+            if (arg[0].equals("on")) {
+                if (state.serverPaused == false) {
+                    state.serverPaused = true;
+                    Call.sendMessage("[#bebebe]Server [green]paused [#bebebe]by [#ffffff] " + player.name + ".");
                 }
-                if (arg[0].equals("off")) {
-                    if (state.serverPaused == true) {
-                        state.serverPaused = false;
-                        Call.sendMessage("[#bebebe]Server [red]unpaused [#bebebe]by [#ffffff] " + player.name + ".");
-                    }
-                    else if (state.serverPaused == false) {
-                        player.sendMessage("[scarlet]Server is already unpaused.");
-                    }
+                else if (state.serverPaused == true) {
+                    player.sendMessage("[scarlet]Server is already paused.");
                 }
-                if (!(arg[0].equals("on") || arg[0].equals("off"))) {
-                    player.sendMessage("[scarlet]Need argument 'on' or 'off'.");
+            }
+            if (arg[0].equals("off")) {
+                if (state.serverPaused == true) {
+                    state.serverPaused = false;
+                    Call.sendMessage("[#bebebe]Server [red]unpaused [#bebebe]by [#ffffff] " + player.name + ".");
                 }
-            });
-        }
+                else if (state.serverPaused == false) {
+                    player.sendMessage("[scarlet]Server is already unpaused.");
+                }
+            }
+            if (!(arg[0].equals("on") || arg[0].equals("off"))) {
+                player.sendMessage("[scarlet]Need argument 'on' or 'off'.");
+            }
+        });
+    }
+    
+    public void registerServerCommands(CommandHandler handler) {
         
-        @Override
-        public void registerServerCommands(CommandHandler handler) {
-            
-        }
+    }
     }
 }
