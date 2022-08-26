1993fd26a203ee483f00b5a6efb3e2977a36898b
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-26 04:20:41 +0200

    update Commands.java

--- core/src/extra/Commands.java
+++ core/src/extra/Commands.java
@@ -1,7 +1,78 @@
-package extra.command;
+package extra.commands;
+
+import arc.Events;
+import arc.func.Cons;
+import arc.util.CommandHandler;
+import arc.util.Log;
+import arc.util.Timer;
+import arc.util.CommandHandler.CommandRunner;
+import mindustry.mod.*;
+import mindustry.game.EventType;
+import mindustry.core.GameState;
+import mindustry.gen.Call;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
+import mindustry.net.Administration;
+import static mindustry.Vars.player;
+import static mindustry.Vars.state;
+import static mindustry.Vars.netServer;
+import static mindustry.Vars.netClient;
 
 public class Commands {
+    public Command {
+        
+    }
     public void load() {
         
+        public void init(){
+            //TODO Remove after official update
+            Events.on(EventType.WorldLoadEvent.class, e -> {
+                if (state.serverPaused == false && Groups.player.size() == 0) {
+                    state.serverPaused = true;
+                    Log.info("[E-U] |--> The server was automatically paused.");
+                }
+            });
+            Events.on(EventType.PlayerJoin.class, e -> {
+                if (state.serverPaused == true && Groups.player.size() == 1) {
+                    state.serverPaused = false;
+                    Log.info("[E-U] |--> The server was automatically unpaused.");
+                    Call.sendMessage("[#bebebe]Server was [red]unpaused.");
+                }
+            });
+            Events.on(EventType.PlayerLeave.class, e -> {
+                if (state.serverPaused == false && Groups.player.size()-1 == 0) {
+                    state.serverPaused = true;
+                    Log.info("[E-U] |--> The server was automatically paused.");
+                }
+            });
+        }
+    
+        @Override
+        public void registerClientCommands(CommandHandler handler) {
+            handler.<Player>register("pause", "<on/off>", "Pause/Unpause the game.", (arg, player) -> {
+                if (arg[0].equals("on")) {
+                    if (state.serverPaused == false) {
+                        state.serverPaused = true;
+                        Call.sendMessage("[#bebebe]Server [green]paused [#bebebe]by [#ffffff] " + player.name + ".");
+                    }
+                    else if (state.serverPaused == true) {
+                        player.sendMessage("[scarlet]Server is already paused.");
+                    }
+                }
+                
+                if (arg[0].equals("off")) {
+                    if (state.serverPaused == true) {
+                        state.serverPaused = false;
+                        Call.sendMessage("[#bebebe]Server [red]unpaused [#bebebe]by [#ffffff] " + player.name + ".");
+                    }
+                    else if (state.serverPaused == false) {
+                        player.sendMessage("[scarlet]Server is already unpaused.");
+                    }
+                }
+                if (!(arg[0].equals("on") || arg[0].equals("off"))) {
+                    player.sendMessage("[scarlet]Need argument 'on' or 'off'.");
+                }
+            });
+        }
     }
 }
